09d079624599266549102ea2804eb1d3aaafc8dc
cipheraenys <68282629+cipheraenys@users.noreply.github.com>
2026-06-05 20:17:01 +0700

    fix(extension): replace clearInterval with clearTimeout and refactor state management

--- extension.js
+++ extension.js
@@ -1,46 +1,49 @@
+// Fork of hide-dock-in-overview@roslax by RosGtHub
+// https://github.com/RosGtHub/Hide-dock-in-overview
+
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-
-let delay = 200;
-let timeout = null;
-
-function onOverviewActivated() {
-  Main.overview.dash.hide();
-}
-
-function onOverviewDeactivated() {
-  timeout = setTimeout(() => { 
-    Main.overview.dash.show();
-  }, delay);
-}
+import GLib from 'gi://GLib';
 
 export default class DockExtension {
   constructor() {
-    this.showingSignalId = null;
-    this.hiddenSignalId = null;
+    this._showingSignalId = null;
+    this._hidingSignalId = null;
+    this._timeoutId = 0;
+    this._delay = 200;
   }
 
   enable() {
-    if (this.showingSignalId) {
-      Main.overview.disconnect(this.showingSignalId);
-    }
-    if (this.hiddenSignalId) {
-      Main.overview.disconnect(this.hiddenSignalId);
-    }
-    this.showingSignalId = Main.overview.connect('showing', onOverviewActivated);
-    this.hiddenSignalId = Main.overview.connect('hiding', onOverviewDeactivated);
+    this._showingSignalId = Main.overview.connect('showing', () => {
+      this._cancelTimeout();
+      Main.overview.dash.hide();
+    });
+
+    this._hidingSignalId = Main.overview.connect('hiding', () => {
+      this._timeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, this._delay, () => {
+        Main.overview.dash.show();
+        this._timeoutId = 0;
+        return GLib.SOURCE_REMOVE;
+      });
+    });
   }
 
   disable() {
-    if (this.showingSignalId) {
-      Main.overview.disconnect(this.showingSignalId);
-      this.showingSignalId = null;
+    if (this._showingSignalId) {
+      Main.overview.disconnect(this._showingSignalId);
+      this._showingSignalId = null;
     }
-    if (this.hiddenSignalId) {
-      Main.overview.disconnect(this.hiddenSignalId);
-      this.hiddenSignalId = null;
+    if (this._hidingSignalId) {
+      Main.overview.disconnect(this._hidingSignalId);
+      this._hidingSignalId = null;
     }
-    if(timeout){
-      clearInterval(timeout);
+    this._cancelTimeout();
+    Main.overview.dash.show();
+  }
+
+  _cancelTimeout() {
+    if (this._timeoutId) {
+      GLib.source_remove(this._timeoutId);
+      this._timeoutId = 0;
     }
   }
 }
